462abcb330d028cc18bd608fbc3ab8411bd9adbf
David Crawford <swancrawford@msn.com>
2022-09-05 14:58:18 +0200

    Initial upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-<<<<<<< HEAD
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,9 +7,3 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-=======
-app = Flask(__name__)@app.route("/")
-def joke():
-    return "I haven't slept for three days, because that would be too long"if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
->>>>>>> 14662b248222879fd614a167ff59e05965df5373
